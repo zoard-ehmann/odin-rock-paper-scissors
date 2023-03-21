3415c828d263551b208b22b0196b8207d5bc987d
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-21 20:44:16 +0100

    feat: Generate random computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+
+  switch (computerChoice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
